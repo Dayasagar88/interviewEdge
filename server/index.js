b99319bc66b387383c7aac6a19efd00baa3cdb88
Daya Sagar <152199702+Dayasagar88@users.noreply.github.com>
2026-03-03 16:58:58 +0530

    Refactor code structure for improved readability and maintainability

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-   origin : "https://interview-edge-seven.vercel.app/",
+   origin : "https://interview-edge-seven.vercel.app/"  || "http://localhost:5173/",
    credentials : true
 }))
 
